21a0f6fbb65aebfd03b75f56befb681cbb4af2a8
stpack <sattha.prat@hotmail.com>
2024-10-27 14:17:02 +0700

    Update package dependencies and add Luxon library

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,24 @@
-import classNames, { ArgumentArray } from 'classnames'
-import { twMerge } from 'tailwind-merge'
+import classNames, { ArgumentArray } from "classnames";
+import { twMerge } from "tailwind-merge";
+import { DateTime } from "luxon";
 
 export function cn(...args: ArgumentArray) {
-    return twMerge(classNames(...args))
-  }
+  return twMerge(classNames(...args));
+}
+
+export function formatDate(
+  date: string | Date | undefined,
+  format = "dd LLLL yyyy"
+) {
+  if (!date) return "-";
+  const newDate = DateTime.fromISO(new Date(date).toISOString());
+  return newDate.toFormat(format);
+}
+
+export function dataFromMillis(
+  date: number | undefined,
+  format = "dd LLLL yyyy"
+) {
+  if (!date) return "-";
+  return DateTime.fromMillis(date).toFormat(format);
+}
