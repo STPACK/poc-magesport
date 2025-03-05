334447a1ae5a336cbe5da02c7bddaf83b5ff5e67
stpack <sattha.prat@hotmail.com>
2025-03-05 17:15:18 +0700

    Feat: enhance TaxInvoicePage with PDF rendering and download functionality; update styles for toolbar

--- src/feature/taxInvoice/pages/TaxInvoicePage/interface.ts
+++ src/feature/taxInvoice/pages/TaxInvoicePage/interface.ts
@@ -1,7 +1,23 @@
-export interface WithTaxInvoicePageProps {
-className?: string
-}
+import React from "react";
 
 export interface TaxInvoicePageProps {
-className?: string
+  invoiceId: string;
+  updateQueryStrings: (
+    newParams: Partial<{
+      invoiceId: string;
+    }>
+  ) => void;
+  isLoading: boolean;
+  data: UploadedFile[] | undefined;
+  images: string[];
+  onDownload: () => void;
+  setCurrent: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export interface UploadedFile {
+  id: string;
+  name: string;
+  originalName: string;
+  url: string;
+  createdAt: number;
 }
